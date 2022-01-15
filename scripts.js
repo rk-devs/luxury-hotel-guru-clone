d49f2ec481cd159947dce5564cc2c0c77628d8ac
errajeevranjan <rjeevrnjn17@gmail.com>
2022-01-16 03:45:32 +0530

    Added swipper to one hotel for testing

---
+++ scripts.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+	zoom: true,
+	pagination: {
+		el: ".swiper-pagination",
+		type: "fraction",
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+});
